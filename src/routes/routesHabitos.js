5c7175afd7edc7e14a9523f45aabb32b705c3b3b
nickolas239 <nickolasdionizio@hotmail.com>
2023-11-15 19:15:58 -0300

    adicionando metodos patch e delete de habitos

--- src/routes/routesHabitos.js
+++ src/routes/routesHabitos.js
@@ -9,6 +9,8 @@ const controllerHabitos = require('../controllers/controllerHabitos')
 router.get('/healthcheck', controllerHabitos.healthCheck)
 router.post('/habitos', controllerHabitos.createHabito)
 router.get('/habitos', controllerHabitos.getAllHabitos)
+router.patch('/habitos/:id', controllerHabitos.updateHabito)
+router.delete('/habitos/:id', controllerHabitos.deleteHabito)
 
 //export { router }
 module.exports = router
